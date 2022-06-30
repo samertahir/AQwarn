9d883592a5b8d0d7a4a9ffc8ecc430a95cadb0b3
samertahir <102942156+samertahir@users.noreply.github.com>
2022-06-30 15:03:52 +0500

    edit change func

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,10 +9,10 @@ use Illuminate\Support\Facades\Gate;
 
 class ProductController extends Controller
 {
-    //public function __construct()
-    //{
-      //  $this->middleware('auth');
-    //}
+    public function __construct()
+    {
+       $this->middleware('auth');
+    }
 
     public function product ()
     {
