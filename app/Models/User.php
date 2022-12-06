abdb36dbc5c77b884acec265f69c60675c006add
rkmughal487 <102944924+rkmughal487@users.noreply.github.com>
2022-12-06 11:41:43 +0500

    rename path changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -50,9 +51,5 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    protected $append=['id_hash','device'];
-    public function getDeviceAttribute(){
-        Device::where($this->id .'-'. $this->status)->get();
-    }
 
-}
+    }
